f3201200ed3851d640e547e2befe4ba360a8b901
yongchanson <syc9430@gmail.com>
2022-01-01 16:30:59 +0900

    router structure  update

--- server.js
+++ server.js
@@ -2,37 +2,37 @@
 
 const express = require('express');
 const path = require('path');
-const movieRouter = express.Router();
+// const movieRouter = express.Router();
 
-let initial_path = path.join(__dirname, "movie");
+let initial_path = path.join(__dirname, 'resources');
 
 let app = express();
 
-app.use(express.static(initial_path));
+// app.use(express.static(initial_path));
+app.use('/static', express.static('resources'));
 
-
-app.get("/:id[a-z]", (req, res) => {
-    res.sendFile(path.join(initial_path, "person.html"));
-})
-// movieRouter.get("person/:id", (req, res) => {
+// app.get("/:id[a-z]", (req, res) => {
 //     res.sendFile(path.join(initial_path, "person.html"));
 // })
+
+app.get("/person/:id", (req, res) => {
+    res.sendFile(path.join(initial_path, "person.html"));
+})
 
 
 // app.get('/:id[0-9]', (req, res) => {
 //     res.sendFile(path.join(initial_path, "detail.html"));
 // })
-app.use("/:id", (req, res) => {
-    return res.sendFile(path.join(initial_path, "detail.html"));
+app.get("/movies/:id", (req, res) => {
+    res.sendFile(path.join(initial_path, "detail.html"));
 })
 
-
-// app.get('/search', (req, res) => {
-//     res.sendFile(path.join(initial_path, "search.html"));
-// })
+app.get('/search', (req, res) => {
+    res.sendFile(path.join(initial_path, "search.html"));
+})
 
 app.get('/', (req, res) => {
-    return res.sendFile(path.join(initial_path, "home.html"));
+    res.sendFile(path.join(initial_path, "home.html"));
 })
 
 app.use((req, res) => {
